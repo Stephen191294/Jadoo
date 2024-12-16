3cce469a24abf2d1836efa0a7b491e85d0c76962
Stephen191294 <150807027+Stephen191294@users.noreply.github.com>
2024-12-16 10:43:52 -0300

    update

--- assets/js/jadoo.js
+++ assets/js/jadoo.js
@@ -6,3 +6,14 @@ function myFunction() {
 
 
 new WOW().init();
+
+
+
+document.querySelector('a[href^="#"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  const target = document.querySelector(this.getAttribute('href'));
+  target.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+});
